7a1c1402a7375fdc30b74f2f812d689f277a6603
sophie-tistyk <sophie.tistyk@gmail.com>
2023-07-02 19:40:40 +0300

    main commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Woman woman = new Woman("Anna", "Martina", 25, "Smith");
+        Man man = new Man("John", "Martina", 29);
+
+        if (woman.getLastName().equals(man.getLastName())) {
+            woman.deregisterPartnership(true);
+        } else woman.registerPartnership(man);
+
+    }
+}
